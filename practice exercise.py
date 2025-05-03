38fc65c34ab6f2c7d08c3fee70f603f07b7d0682
nuamah toffick <tofficknuamahrafat.com>
2025-05-03 06:37:42 -0800

    second python commit

--- practice exercise.py
+++ practice exercise.py
@@ -1,34 +1,36 @@
 
+#arithmetic operations
+
+a = float(input("Enter your number?"))
+b = float(input("Enter your number?"))
+total = a + b
+total = a * b
+total = a / b
+total = a // b
+total = a - b
+total = a ** b
+total = a % b
+print(total)
+
+#comparing numbers
+a = 10
+b = 15
+total = a == b
+total = a != b
+total = a > b
+total = a < b
+total = a <= b
+total = a >= b
+print(total)
+
+#using logical operators to combine 
+
+Q = 20
+print(Q > 5 and Q < 30)
+print(Q < 30 and Q > 5)
+print(not(Q < 5))
 
 
-#My favorite food
-
-foodList = ["Banku with Okra", "Rice with Tomatoes stew","Fufu with Groundnut soup",
-            "Yam with Taro leaves stew","Cocoyam with light soup"]
-print(foodList)
-
-print("Rice with Tomatoes stew" in foodList)
-
-myFavorite = input("Enter a food: ")
-
-if myFavorite in foodList:
-    print("yes! it is in the list.")
-
-else:
-    print("sorry, it is not in the list.")
-
-
-#My dictionary
-
-aboutMe = {"name": "Toffick", "age": "95", "city": "Takoradi",
-           "language": "python", "hobbies": "coding"}
-print(aboutMe["name"])
-
-aboutMe["Occupation"]= "programmer"
-print(aboutMe)
-
-aboutMe["name"]= "Hanshimi"
-print(aboutMe)
 
 
 
